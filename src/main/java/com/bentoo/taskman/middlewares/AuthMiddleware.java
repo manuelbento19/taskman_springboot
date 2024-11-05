7fb1906439ca3a4a12695b3e99d108b0edc96619
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-11-05 10:54:38 +0100

    feat: validate routes on authmiddleware

--- src/main/java/com/bentoo/taskman/middlewares/AuthMiddleware.java
+++ src/main/java/com/bentoo/taskman/middlewares/AuthMiddleware.java
@@ -21,40 +21,44 @@ public class AuthMiddleware extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader==null){
-            response.sendError(401);
-            return;
-        }
-        String[] parts = authorizationHeader.split(" ");
-        if(parts.length!=2){
-            response.sendError(401,"Invalid token");
-            return;
-        }
-        String shema = parts[0], token = parts[1];
-        if(!shema.matches("Basic")){
-            response.sendError(401,"Invalid token schema");
-            return;
-        }
+        String path = request.getServletPath();
+        if(path.equals("/task")){
+            String authorizationHeader = request.getHeader("Authorization");
+            if(authorizationHeader==null){
+                response.sendError(401);
+                return;
+            }
+            String[] parts = authorizationHeader.split(" ");
+            if(parts.length!=2){
+                response.sendError(401,"Invalid token");
+                return;
+            }
+            String shema = parts[0], token = parts[1];
+            if(!shema.matches("Basic")){
+                response.sendError(401,"Invalid token schema");
+                return;
+            }
 
-        byte[] decoded = Base64.getDecoder().decode(token);
-        String[] auth = new String(decoded).split(":");
-        if(auth.length!=2){
-            response.sendError(401,"Invalid token format");
-            return;
-        }
+            byte[] decoded = Base64.getDecoder().decode(token);
+            String[] auth = new String(decoded).split(":");
+            if(auth.length!=2){
+                response.sendError(401,"Invalid token format");
+                return;
+            }
 
-        String email = auth[0], password = auth[1];
-        User userExists = userRepository.findByEmail(email);
-        if(userExists==null){
-            response.sendError(401,"User doesn't exists");
-            return;
-        }
+            String email = auth[0], password = auth[1];
+            User userExists = userRepository.findByEmail(email);
+            if(userExists==null){
+                response.sendError(401,"User doesn't exists");
+                return;
+            }
 
-        var compare = BCrypt.verifyer().verify(password.toCharArray(),userExists.password.toCharArray());
-        if(!compare.verified){
-            response.sendError(401,"User doesn't exists");
-            return;
+            var compare = BCrypt.verifyer().verify(password.toCharArray(),userExists.password.toCharArray());
+            if(!compare.verified){
+                response.sendError(401,"User doesn't exists");
+                return;
+            }
+            filterChain.doFilter(request,response);
         }
         filterChain.doFilter(request,response);
     }
